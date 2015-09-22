e77a5bb6fec22a847c0f3e364a6eb72b59503cb6
Joel Fischer <jfjoel.fischer@gmail.com>
2015-09-22 13:46:06 +0200

    Initial Commit

---
+++ WhatsPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhatsPad
+//
+//  Created by Joel Fischer on 22/09/15.
+//  Copyright © 2015 Joel Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WhatsPad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WhatsPad
+//
+//  Created by Joel Fischer on 22/09/15.
+//  Copyright © 2015 Joel Fischer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
